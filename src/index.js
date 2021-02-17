90c97a8d0ca5ff18e9912d260cb2a1f2f3ac502c
Jawad <approvaltrick@gmail.com>
2021-02-17 16:33:18 +0500

    Multiple images export to dist with example using loop inside pug file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import ico from "./images/favicon.ico";
+// import ico from "./images/favicon.ico";
 import "../node_modules/bootstrap/scss/bootstrap.scss";
 import "./sass/theme.scss";
 
 import "../node_modules/bootstrap/dist/js/bootstrap.js";
 import "./js/custom.js";
+
+// Get all required images
+require.context("./images", false, /\.(png|jpe?g|svg|ico)$/);
